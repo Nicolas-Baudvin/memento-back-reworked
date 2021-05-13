350ef9dc8c2e52113884f644014badb62dca6b46
unknown <nicolas.baudvin@gmail.com>
2021-05-13 14:08:09 +0200

    Tasks routes

--- Routes/tasks.js
+++ Routes/tasks.js
@@ -1,12 +1,39 @@
-const { true } = require("tap");
 const createTask = require("../Controller/Tasks/create");
-const { createTasksBodySchema } = require("../RoutesSchema/Tasks");
+const deleteTask = require("../Controller/Tasks/delete");
+const patchTaskDesc = require("../Controller/Tasks/patchDesc");
+const patchTaskImportance = require("../Controller/Tasks/patchImportance");
+const patchTasksOrder = require("../Controller/Tasks/patchOrder");
+const { createTaskBodySchema, deleteTaskBodySchema, patchTaskDescBodySchema, patchTaskImportanceBodySchema, patchTasksOrderBodySchema } = require("../RoutesSchema/Tasks");
 
 const tasksRoutes = (fastify, opts, next) => {
     fastify.post(
         "/api/tasks/create/",
-        { schema: createTasksBodySchema, attachValidation: true, preValidation: [fastify.authenticate] },
+        { schema: createTaskBodySchema, attachValidation: true, preValidation: [fastify.authenticate] },
         createTask
+    );
+
+    fastify.delete(
+        "/api/tasks/delete/",
+        { schema: deleteTaskBodySchema, preValidation: [fastify.authenticate] },
+        deleteTask
+    );
+
+    fastify.patch(
+        "/api/tasks/patch/desc/",
+        { schema: patchTaskDescBodySchema, attachValidation: true, [fastify.authenticate] },
+        patchTaskDesc
+    );
+
+    fastify.patch(
+        "/api/tasks/patch/importance/",
+        { schema: patchTaskImportanceBodySchema, preValidation: [fastify.authenticate] },
+        patchTaskImportance
+    );
+
+    fastify.patch(
+        "/api/tasks/patch/order/",
+        { schema: patchTasksOrderBodySchema, preValidation: [fastify.authenticate] },
+        patchTasksOrder
     );
     
     next();
