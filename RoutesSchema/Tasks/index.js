7ad7ba2d4da32c7967f95560277b7941d7297485
unknown <nicolas.baudvin@gmail.com>
2021-05-13 14:08:30 +0200

    Tasks routes schema

--- RoutesSchema/Tasks/index.js
+++ RoutesSchema/Tasks/index.js
@@ -1,6 +1,6 @@
 const { authHeaders } = require("../User");
 
-exports.createTasksBodySchema = {
+exports.createTaskBodySchema = {
     body: {
         type: "object",
         properties: {
@@ -10,6 +10,69 @@ exports.createTasksBodySchema = {
             _id: { type: "string" },
         },
         required: ["desc"],
+    },
+    headers: authHeaders
+};
+
+exports.deleteTaskBodySchema = {
+    body: {
+        type: "object",
+        properties: {
+            taskID: { type: "string" },
+            boardID: { type: "string" },
+            listID: { type: "string" },
+        },
+    },
+    headers: authHeaders
+};
+
+exports.patchTaskImportanceBodySchema = {
+    body: {
+        type: "object",
+        properties: {
+            taskID: { type: "string" },
+            boardID: { type: "string" },
+            listID: { type: "string" },
+            importance: { type: "boolean" },
+        },
+    },
+    headers: authHeaders
+}
+
+exports.patchTaskDescBodySchema = {
+    body: {
+        type: "object",
+        properties: {
+            taskID: { type: "string" },
+            boardID: { type: "string" },
+            listID: { type: "string" },
+            newDesc: { type: "string" },
+        },
+    },
+    headers: authHeaders
+}
+
+exports.patchTaskAuthorBodySchema = {
+    body: {
+        type: "object",
+        properties: {
+            taskID: { type: "string" },
+            boardID: { type: "string" },
+            listID: { type: "string" },
+            author: { type: "string" },
+        },
+    },
+    headers: authHeaders
+}
+
+exports.patchTasksOrderBodySchema = {
+    body: {
+        type: "object",
+        properties: {
+            sourceList: { type: "object" },
+            boardID: { type: "string" },
+            tasks: { type: "object" },
+        }
     },
     headers: authHeaders
 };
